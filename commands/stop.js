3b77c948a1c62c710e190e0789df700ea1befb4b
Brian van Rheenen <b.van.rheenen@bmcr.nl>
2019-09-27 16:50:30 +0200

    Fixed error when trying to stop the bot if it's not in a voice channel.

--- commands/stop.js
+++ commands/stop.js
@@ -2,6 +2,11 @@ module.exports = {
 	name: 'stop',
 	description: 'Stop a stream.',
 	execute(message, args) {
+		if (!message.member.voiceChannel) {
+			message.channel.send("I am currently not connected to any voice channels.");
+			return;
+		}
+		
     message.member.voiceChannel.leave();
 	},
 };
