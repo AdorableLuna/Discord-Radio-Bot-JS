835b4ab8b3404ab5a19769b4dfaaed9fd9990a15
Brian van Rheenen <b.van.rheenen@bmcr.nl>
2019-09-27 16:46:56 +0200

    Moved initialization of commands to its own function.

--- bot.js
+++ bot.js
@@ -5,13 +5,7 @@ const ConfigFile = require("./config");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-
-	client.commands.set(command.name, command);
-}
+initializeCommands();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -33,5 +27,15 @@ client.on("message", message => {
 		console.error(error);
 	}
 })
+
+function initializeCommands() {
+	const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const command = require(`./commands/${file}`);
+
+		client.commands.set(command.name, command);
+	}
+}
 
 client.login(ConfigFile.token);
