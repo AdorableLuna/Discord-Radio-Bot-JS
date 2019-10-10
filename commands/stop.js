8ca1a3f1985e6dd033acf0b7ddcc097dfa68e2ae
Brian van Rheenen <b.van.rheenen@bmcr.nl>
2019-10-10 16:44:29 +0200

    Added checks for /stop command to check if user or/and bot is in the channel.

--- commands/stop.js
+++ commands/stop.js
@@ -1,12 +1,23 @@
+const ConfigFile = require("../config");
+
 module.exports = {
 	name: 'stop',
 	description: 'Stop a stream.',
 	execute(message, args) {
 		if (!message.member.voiceChannel) {
-			message.channel.send("I am currently not connected to any voice channels.");
+			message.channel.send("You are currently not connected to any voice channels.");
 			return;
 		}
-		
+
+		if (message.member.voiceChannel != null) {
+			const bot = message.member.voiceChannel.members.find(user => user.id == ConfigFile.clientId);
+
+			if (!bot) {
+				message.channel.send(`I am currently not connected to ${message.member.voiceChannel.name} voice channel.`);
+				return;
+			}
+		}
+
     message.member.voiceChannel.leave();
 	},
 };
